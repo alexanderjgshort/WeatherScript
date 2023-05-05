e6bf0f86008b42cc25512b78c80c2ff3729880e9
Alexander Short <alexandershort19@gmail.com>
2023-05-05 12:54:46 -0400

    Modified scipt to also gather ULO rates and times

--- OntarioTOU.py
+++ OntarioTOU.py
@@ -8,10 +8,18 @@ data = soup.find_all('td')
 
 for i in range(1,4):
 
-    print(data[(i * 4) - 4].text)                          #Peak title
-    print(data[(i * 4) - 1].text)                          #Rate (cents/kWh)
+    print(data[(i * 4) - 4].text)                          #TOU Price Periods
+    print(data[(i * 4) - 1].text)                          #TOU Prices (cents/kWh)
     print('Summer:')
-    print(data[(i * 4) - 2].text.replace('\t',''))         #Time (Summer)
+    print(data[(i * 4) - 2].text.replace('\t',''))         #Times (Summer)
     print('Winter:')
-    print(data[(i * 4) - 3].text.replace('\t',''))         #Time (Winter)
+    print(data[(i * 4) - 3].text.replace('\t',''))         #Times (Winter)
+    print()
+    a = i*4
+
+for j in range(1,5):
+
+    print(data[(j * 3) + a - 3].text)                       #ULO Price Periods
+    print(data[(j * 3) + a - 2].text)                       #Times (All Year)
+    print(data[(j * 3) + a - 1].text)                       #ULO Prices (cents/kWh)
     print()
